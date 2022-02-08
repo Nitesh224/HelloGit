ee07dfd94af6c611fc5ac7c2d6f681833cac2142
Nitesh224 <jhanitesh8981@gmail.com>
2022-02-08 23:16:46 +0530

    nodejs

---
+++ index.js
@@ -0,0 +1,47 @@
+// starting JS -> JavaScript
+// var let const
+//variable declaration
+var a;// int string float , it can be anythhing
+
+//variable initialization
+a=10;
+console.log(typeof(a));
+console.log(a);
+
+a ="hello";// reintialize of variable a
+console.log(a);
+
+//typeof operator-> it tells us the type  data stored in variable a;
+a=true;
+console.log(a);
+console.log(typeof a)
+
+a= null;
+console.log(a);
+console.log(typeof a);
+//Number
+
+var num = 10;
+console.log(num);
+num = 2.4;
+console.log(num);
+ //Boolean
+ var t=true;
+ var f=false;
+ console.log(t);
+ console.log(f)
+ //Strings
+ // "", '',``
+ //single quotes ,double quotes, backtick
+ var str = 'a';
+ console.log(typeof str);
+ var str1="How you doin ?\ni am fine.How about you ?"
+ //backslash n("\n")
+ var b=`hi hope you guys are able to 
+ grasp the concept`;
+ console.log(str1);
+ console.log(b);
+ var num = 200;
+ console.log(`half of ${num} is ${num/2}`);
+ // computing is done first inside the curly braces then string is printed.
+ 
