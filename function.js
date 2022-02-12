5cfdb448725e1af2fe60625187ef0c3dde9fec18
Nitesh224 <jhanitesh8981@gmail.com>
2022-02-12 22:55:24 +0530

    done

---
+++ function.js
@@ -0,0 +1,35 @@
+// 3 types of function
+//Normal function
+//function defination
+//function function_name(param1,param2){
+    //do something
+//}
+//function invoke
+//function_name(arg1,arg2)
+function add(a,b){
+    return a+b;
+}
+let ans = add(2,6);
+console.log(ans);
+//functions are treated as first class citizens in javascript
+//->function can be returned
+//->functions can be passes as parameters
+ //function expression
+function calculator(str,a,b){
+    if(str=='add'){
+        return function add(){
+            console.log(a+b);
+        }
+
+    }
+    //else if(str=='sub'){
+
+   // }
+}
+let returnedfunc=calculator('add',2,3);
+console.log(returnedfunc);
+returnedfunc();
+let sayHi=function(){
+    console.log("Hello guys");
+}
+sayHi();
